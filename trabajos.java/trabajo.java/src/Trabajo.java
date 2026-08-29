6978cbdf796b5fcddd01dc149993b505bf94f2ac
leandro15kener <kenerleandrobecerraquintero@gmail.com>
2026-08-29 07:38:06 -0500

    Add files via upload

---
+++ trabajos.java/trabajo.java/src/Trabajo.java
@@ -0,0 +1,11 @@
+public class Trabajo {
+    public static void main(String[] args) {
+        String greeting = "kener";
+        System.out.println(greeting);
+        char letter = 'K';
+        System.out.println(letter);
+    }
+}
+    
+
+
